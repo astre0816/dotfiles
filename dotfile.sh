4fde1bfe5ab03a06ebfcf63e3323cdd8f858a7bd
astre0816 <astre0816.dev@gmail.com>
2020-04-15 18:39:22 +0900

    anywhere dotfiles

---
+++ dotfile.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd `dirname $0`
+ln -sf .config/nvim ~/.config/nvim
+ln -sf .config/nvim ~/.vim
+ln -sf .config/nvim/init.vim ~/.vimrc
--- dotfilesLink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ln -sf ~/dotfiles/.config/ ~/.config
-ln -sf ~/.config/nvim ~/.vim
-ln -sf ~/.config/nvim/init.vim ~/.vimrc
